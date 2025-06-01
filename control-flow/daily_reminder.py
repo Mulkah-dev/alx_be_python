fa7073b503a28a14bd5884a7da83d6436b1ba2b1
mabdullahi <mabdullahi@appzonegroup.com>
2025-06-01 16:24:32 +0100

    Edits made

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
-task = input("Enter your task:")
-priority = input("Enter the task's priority (high,medium,low):")
-time_bound = input("Is it time-bound? (yes/no):")
+Task = input("Enter your Task:")
+Priority = input("Enter the Task's Priority (high,medium,low):")
+Time_bound = input("Is it time-bound? (yes/no):")
 
-if time_bound == "yes":
-    match priority:
+if Time_bound == "yes":
+    match Priority:
         case "high":
-            print("Reminder:", task, "is a", priority, "priority task that requires immediate action today!")
+            print("Reminder:", Task, "is a", Priority, "Priority Task that requires immediate action today!")
         case "medium":
-            print("Reminder:", task, "is a", priority, "priority task that requires action today!")
+            print("Reminder:", Task, "is a", Priority, "Priority Task that requires action today!")
         case "low":
-            print("Reminder:", task, "is a", priority, "priority task. Consider completing it when you have free time")
+            print("Reminder:", Task, "is a", Priority, "Priority Task. Consider completing it when you have free time")
 else:
-    match priority:
+    match Priority:
         case "high":
-            print("Reminder:", task, "is a", priority, "priority task. Plan to do it soon even though it's not urgent.")
+            print("Reminder:", Task, "is a", Priority, "Priority Task. Plan to do it soon even though it's not urgent.")
         case "medium":
-            print("Reminder:", task, "is a", priority, "priority task. Add it to your schedule for this week.")
+            print("Reminder:", Task, "is a", Priority, "Priority Task. Add it to your schedule for this week.")
         case "low":
-            print("Reminder:", task, "is a", priority, "priority task. Do it when you have spare time.")        
+            print("Reminder:", Task, "is a", Priority, "Priority Task. Do it when you have spare time.")        
 
 
