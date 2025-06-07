9c99c8ce948120a2b4cab08f2a126bab90a83f96
mabdullahi <mabdullahi@appzonegroup.com>
2025-06-07 23:32:37 +0100

    Add edits

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -16,3 +16,38 @@ def display_menu():
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            item_name = input("Enter the item to add: ")
+            append(item_name)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item_name = input("Enter the item name yo want to remove: ")
+            remove(item_name)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(print_list())
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
