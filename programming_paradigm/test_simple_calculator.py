b86cefa9711638d8d0e58893767f44230d497f74
mabdullahi <mabdullahi@appzonegroup.com>
2025-06-14 16:37:06 +0100

    Add test and calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,28 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2,4),6)
+        self.assertEqual(self.calc.add(5,6),11)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5,3),2)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(5,3),15)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10,2),5)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(10,0))
+
+    
+
+if __name__ == '__main':
+    unittest.main()
