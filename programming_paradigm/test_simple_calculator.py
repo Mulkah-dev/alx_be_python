89d03b8a2217ae0b3343b8b35e393806dd1922f4
mabdullahi <mabdullahi@appzonegroup.com>
2025-06-14 17:04:06 +0100

    Update file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,11 +6,11 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2,4),6)
         self.assertEqual(self.calc.add(5,6),11)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3),2)
 
     def test_multiply(self):
