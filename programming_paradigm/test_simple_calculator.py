cc78287d4dd4941bc0d186c95765294bb1075a71
mabdullahi <mabdullahi@appzonegroup.com>
2025-06-14 17:05:44 +0100

    Update changes made

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,13 +13,13 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3),2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5,3),15)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10,2),5)
 
-    def test_divide_by_zero(self):
+    def test_division_by_zero(self):
         self.assertIsNone(self.calc.divide(10,0))
 
     
