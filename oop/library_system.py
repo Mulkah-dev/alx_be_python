31bc0a1bc8fa45114da3f186d6c963bdb0791bdb
mulkah-dev <mulkah.akala@gmail.com>
2025-06-22 09:58:09 +0100

    Add new changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class EBook(Book):
 
     def __str__(self):
         #return super().__str__()
-        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}"
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title=str, author=str, page_count = int):
@@ -23,7 +23,7 @@ class PrintBook(Book):
 
     def __str__(self):
         #return super().__str__()
-        return f"PrintBook: {self.title} by {self.author} ,Page Count: {self.page_count}"
+        return f"PrintBook: {self.title} by {self.author} , Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
